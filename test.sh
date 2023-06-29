d1ea5a1e36d89ef5ac650a61cc092cd9a99db35f
luckydonald <mac2019.git@luckydonald.de>
2023-06-30 00:22:07 +0200

    tests: allow running test with pyenv.

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -e
-python3 -m venv venv
-. ./venv/bin/activate
+python3 -m venv venv || pyenv virtualenv 3.11.4 quart-login_3.11.4
+. ./venv/bin/activate || . $(pyenv root)/versions/quart-login_3.11.4/bin/activate
 python3 -m pip install pip-tools
+python3 -m pip install pip-compile-multi
 pip-compile-multi --directory "./requirements"
 python3 -m  pip install -r "./requirements/dev.txt"
 python3 -m  pip install .
